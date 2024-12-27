c7f4014342309a6bd49e48bff291338db345565f
Zaahiid <zahidrsheikh01@gmail.com>
2024-12-27 22:15:49 +0530

    Added page and Banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,144 @@
-import Image from "next/image";
+"use client";
+import Banner from "../components/Banner";
+import Player from "@madzadev/audio-player";
+import "@madzadev/audio-player/dist/index.css";
+import { useEffect, useState } from "react";
+
+const colors = {
+  tagsBackground: "#3e32e4",
+  tagsText: "#ffffff",
+  tagsBackgroundHoverActive: "#6e65f1",
+  tagsTextHoverActive: "#ffffff",
+  searchBackground: "#18191f",
+  searchText: "#ffffff",
+  searchPlaceHolder: "#575a77",
+  playerBackground: "#000",
+  titleColor: "#16A343",
+  timeColor: "#16A343",
+  progressSlider: "#16A343",
+  progressUsed: "#16A343",
+  progressLeft: "#16A343",
+  bufferLoaded: "#16A343",
+  volumeSlider: "#3e32e4",
+  volumeUsed: "#ffffff",
+  volumeLeft: "#151616",
+  playlistBackground: "#fff",
+  playlistText: "#575a77",
+  playlistBackgroundHoverActive: "#18191f",
+  playlistTextHoverActive: "#ffffff",
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [word, setWord] = useState("hello");
+  const [dictionary, setDictionary] = useState<any>();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      .then(async (res) => {
+        const data = await res.json();
+        setDictionary(data);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [word]);
+
+  const handleKeyPressEvent = (event: React.KeyboardEvent<HTMLInputElement>) =>{
+    if(event.key === "Enter"){
+      event.preventDefault();
+      setWord(event.currentTarget.value);
+    }
+  }
+
+  return (
+    <main>
+      <Banner changeWord={setWord} />
+      <section className="absolute top-[27vh] bg-white w-[80%] ml-[10%] shadow-2xl p-5 rounded-2xl">
+        <div className="flex justify-between">
+          <span className="shadow-md px-6 py-2 rounded-lg bg-green-600 text-white">
+            {" "}
+            <span className="h-[10px] w-[10px] bg-yellow-300 rounded-full inline-block"></span>{" "}
+            {word}
+          </span>
+          <h1 className=" text-xl font-extrabold text-gray-900  md:text-2xl lg:text-3xl">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+              FIND IN
+            </span>{" "}
+            DICTIONARY{" "}
+          </h1>
+          {dictionary?.length && (
+            <span className="shadow-md px-6 py-2 rounded-lg bg-blue-600 text-white">
+              {" "}
+              <span className="h-[10px] w-[10px] bg-blue-300 rounded-full inline-block"></span>{" "}
+              {`phonetic:${dictionary[0]?.phonetics[1]?.text}`}
+            </span>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        <section className="mt-5 pt-4 border-solid border-0 border-t border-gray-300">
+          <span className="shadow-md px-6 py-2 rounded-lg font-semibold bg-white text-green-700 flex items-center justify-between max-w-[120px]">
+            {" "}
+            <span className="h-[10px] w-[10px] bg-yellow-300 rounded-full inline-block"></span>
+            Origin
+          </span>
+          <p className="py-3 text-gray-600 bg-gray-100 mt-4 px-4 rounded-lg">
+            "early 19th century: variant of earlier hollo ; related to holla."
+          </p>
+        </section>
+
+        {dictionary?.length &&
+          dictionary[0]?.meanings.map((data: any, index: number) => (
+            <section
+              key={index}
+              className="mt-8 shadow-2xl p-5 rounded-xl bg-gray-100 "
+            >
+              <span className="shadow-md px-6 py-3 rounded-lg font-semibold bg-gray-600 text-white flex items-center justify-between max-w-[150px]">
+                {" "}
+                <span className="h-[10px] w-[10px] bg-white rounded-full inline-block"></span>
+                MEANING
+              </span>
+
+              <div>
+                <span className="shadow-sm mt-4 px-3 py-2 rounded-full font-semibold bg-white text-green-700 flex items-center justify-between max-w-[330px]">
+                  {" "}
+                  <span className="h-[10px] w-[10px] bg-yellow-300 rounded-full inline-block"></span>
+                  Part of speech{" "}
+                  <span className="shadow-md px-6 py-2 rounded-full font-semibold bg-green-600 text-white flex items-center justify-between max-w-[400px]">
+                    {" "}
+                    {data.partOfSpeech}
+                  </span>{" "}
+                </span>
+                {data.definitions.map((e: any, i: any) => (
+                  <p
+                    key={i}
+                    className="py-3 text-gray-600 bg-white mt-4 px-4 rounded-lg"
+                  >
+                    {e.definition}
+                  </p>
+                ))}
+              </div>
+            </section>
+          ))}
+
+        <div className="mt-4">
+          <Player
+            includeTags={false}
+            includeSearch={false}
+            showPlaylist={false}
+            sortTracks={false}
+            autoPlayNextTrack={false}
+            trackList={[
+              {
+                url: "//ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3",
+                title: word,
+                tags: [word],
+              },
+            ]}
+            customColorScheme={colors}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div>
+      </section>
+    </main>
   );
 }
